78c99e4b2eb44c0cd02129344a046b1bcab14c23
Dore Guzmán <talianamoreno08@gmail.com>
2025-10-19 12:56:27 -0500

    creacion de enums

---
+++ autos-taller/src/enums/enums.ts
@@ -0,0 +1,5 @@
+export enum VehiculoEstado { vendido='vendido', disponible='disponible', reservado='reservado', en_reparacion='en_reparacion' }
+export enum MetodoPago { efectivo='efectivo', transferencia='transferencia', credito='credito', cheque='cheque' }
+export enum ClienteTipo { empresa='empresa', particular='particular' }
+export enum EmpleadoCargo { gerente='gerente', vendedor='vendedor', asesor='asesor' }
+export enum ServicioEstado { pendiente='pendiente', en_proceso='en_proceso', finalizado='finalizado' }
